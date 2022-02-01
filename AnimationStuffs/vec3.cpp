647ef1e53817e1f2a63c9de78ba8e0f59c478eab
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2022-01-31 21:16:11 -0300

    Vector stuffs

---
+++ AnimationStuffs/vec3.cpp
@@ -0,0 +1,13 @@
+#include "vec3.h"
+
+vec3 operator+(const vec3& l, const vec3& r) {
+	return vec3(l.x + r.x, l.y + r.y, l.z + r.z);
+}
+
+vec3 operator-(const vec3& l, const vec3& r) {
+	return vec3(l.x - r.x, l.y - r.y, l.z - r.z);
+}
+
+vec3 operator*(const vec3& v, float f) {
+	return vec3(v.x * f, v.y * f, v.z * f);
+}
